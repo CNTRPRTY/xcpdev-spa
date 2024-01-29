25010a9103d1b23aa9c21d19f426a7a1b33c529c
Juan <dev@juanaleman.com>
2024-01-29 11:08:18 -0600

    search in new style

--- src/routes/shared/search.js
+++ src/routes/shared/search.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { withRouter } from './classhooks';
+import {
+  Button,
+  TextInput,
+} from "@tremor/react";
 
 // move to util if reused
 function isAssetName(possible) {
@@ -101,20 +105,25 @@ class Search extends React.Component {
     const placeholder = " block / tx / address / asset";
     // const placeholder = " block / tx_index / tx_hash / address / asset";
 
-    const placeholder_size = placeholder.length - 10;
+    // const placeholder_size = placeholder.length - 10;
     // const placeholder_size = placeholder.length - 12;
 
     return (
       <>
 
         {/* <span> */}
-        <div style={{ padding: "1.1rem 0 0.5rem 0" }}>
-          <form onSubmit={this.handleSearchSubmit}>
-            <input type="text" value={this.state.search} onChange={this.handleSearchChange} placeholder={placeholder} size={placeholder_size} />
-            {' '}
-            <input type="submit" value={"go"} />
-          </form>
-        </div>
+        {/* <div style={{ padding: "1.1rem 0 0.5rem 0" }}> */}
+
+        <form onSubmit={this.handleSearchSubmit}>
+          <div className={"flex flex-row w-full items-center justify-center space-x-1"}>
+            <div className="flex flex-row w-full max-w-3xl  items-center bg-slate-100 dark:bg-slate-800 p-1 space-x-2 rounded-xl">
+              <TextInput icon={MagnifyingGlassIcon} placeholder={placeholder} className={"w-full border-none dark:border-none focus:ring-none ring-transparent dark:ring-transparent"} value={this.state.search} onChange={this.handleSearchChange} />
+              <Button type="submit" size={"xs"} variant={"primary"} value={"Go"} className={"bg-yellow-600 hover:bg-yellow-800 dark:bg-yellow-600 dark:hover:bg-yellow-800 ring-transparent border-none"}>Go</Button>
+            </div>
+          </div>
+        </form>
+
+        {/* </div> */}
         {/* </span> */}
 
       </>
